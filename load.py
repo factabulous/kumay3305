f33725bb2677601d63b1eb41be73ca8d4b84b2d3
Andy Mell <afmell@afmell.com>
2019-03-12 13:34:39 +0000

    More tweaks to the gui

--- load.py
+++ load.py
@@ -64,11 +64,11 @@ def plugin_app(parent):
 
     this.target_waypoint = tk.StringVar()
     this.target_waypoint.set(this.selected_waypoint)
-    tk.OptionMenu(this.status_frame, this.target_waypoint, *this.waypoints.names()).grid(row=h.row(), column=h.col(), columnspan=4, sticky=tk.W)
+    tk.OptionMenu(this.status_frame, this.target_waypoint, *this.waypoints.names()).grid(row=h.row(), column=h.col(4), columnspan=4, sticky=tk.W)
     h.newrow()
     # Target Heading
-    tk.Label(this.status_frame, text="Target Heading").grid(row=h.row(), column=h.col(), sticky=tk.W)
-    this.target_heading = tk.StringVar()
+    tk.Label(this.status_frame, text="Heading").grid(row=h.row(), column=h.col(), sticky=tk.W)
+    this.target_heading = tk.DoubleVar()
     tk.Label(this.status_frame, textvariable=this.target_heading).grid(row=h.row(), column=h.col(), sticky=tk.W)
     # Distance
     tk.Label(this.status_frame, text="Distance").grid(row=h.row(), column=h.col(), sticky=tk.W)
